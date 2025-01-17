3648f225888b28d988f99e4ae899604e454f2802
san7842 <sandeepjjjjj536@gmail.com>
2025-01-17 23:42:48 +0530

    add

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
-"print("hello")
+import keyword
+# x=keyword.kwlist#35 keyword False,True,None is capital and other smalle
+# print(x)
+# print(len(x))
+y=keyword.softkwlist#4('_','case','match','type')
+print(y)
+print(len(y))
+# z=keyword.iskeyword# built in method contain of frozenset object 
+# print(z)
+# a=
+# print(a)
+
+# print("hello")
+# l=[1,2,33,5]
+# for i in range(len(l)):
+#    print(i)
