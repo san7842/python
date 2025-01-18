a2a25f97dc9787b70a3818a69a23c91a51e0210e
san7842 <sandeepjjjjj536@gmail.com>
2025-01-18 22:54:37 +0530

    add

--- demo.py
+++ demo.py
@@ -1,16 +1,39 @@
-import keyword
-# x=keyword.kwlist#35 keyword False,True,None is capital and other smalle
+# import keyword
+# # x=keyword.kwlist#35 keyword False,True,None is capital and other smalle
+# # print(x)
+# # print(len(x))
+# y=keyword.softkwlist#4('_','case','match','type')
+# print(y)
+# print(len(y))
+# # z=keyword.iskeyword# built in method contain of frozenset object 
+# # print(z)
+# # a=
+# # print(a)
+#===string=====
+# import string
+# spe_chr=string.punctuation
+# print(spe_chr)
+# print(len(spe_chr))
+# x=string.ascii_letters
 # print(x)
-# print(len(x))
-y=keyword.softkwlist#4('_','case','match','type')
-print(y)
-print(len(y))
-# z=keyword.iskeyword# built in method contain of frozenset object 
+# y=string.ascii_lowercase
+# print(y)
+# z=string.ascii_uppercase
 # print(z)
-# a=
-# print(a)
-
-# print("hello")
-# l=[1,2,33,5]
-# for i in range(len(l)):
-#    print(i)
+# p=string.digits
+# print(p)
+# q=string.hexdigits
+# print(q)
+# r=string.printable
+# print(r)
+# ===identifier====
+# 1x=10
+# print(1x)# invailid decimal literal  syntaxerror show
+x=10
+print(x)
+_x=20
+print(_x)
+# y=30
+# print(Y)#nameerror show and not define
+x="hello"
+print(x)
