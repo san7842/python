6bfb29205486371b69e7edf86152346dab96f0f4
san7842 <sandeepjjjjj536@gmail.com>
2025-01-20 23:52:12 +0530

    add

--- day1.py
+++ day1.py
@@ -1,11 +1,22 @@
-a=5
-b=3
-# print(bin(a))
-# print(bin(b))
-# print(bin(b-a))
-print(a-b)
-print(a+b)
-print(a/b)
-print(a//b)
-print(a%b)
-print(a*b)
+# a=5
+# b=3
+# # print(bin(a))
+# # print(bin(b))
+# # print(bin(b-a))
+# print(a-b)
+# print(a+b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a*b)
+# x=10
+# y=20
+# z=30
+# print(x>y and y>z) 
+# str1="neeraj"
+# print('n' in str1 )
+x=[10]
+y=[10]
+print(id(x))
+print(id(y))
+print(x is y)# object check address by identity operator is , is not
