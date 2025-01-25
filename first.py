920eb7d6748d8165596aa07b97ef6296eb84c13a
san7842 <sandeepjjjjj536@gmail.com>
2025-01-25 23:10:32 +0530

    add

--- first.py
+++ first.py
@@ -1,6 +1,17 @@
-print("hello")
-print(10+20)
-a=5
-b=10
-c=a+b
-print(type(c))
+# print("hello")
+# print(10+20)
+# a=5
+# b=10
+# c=a+b
+# print(type(c))
+# a=10
+# b=20
+# print(a&b)
+# print(a|b)
+# print(~a)
+x=10
+y=20
+# print(x^y)
+# print(x<<3)
+print(10>>2)
+print(y>>2)
---
+++ slice.py
@@ -0,0 +1,11 @@
+# s1='python'
+# print( s1[ : ])
+
+# s1='python'
+# print( s1[ : :])
+
+# s1='python'
+# print( s1[ : :-1])
+
+s2="ilovepython"
+print(s2[ 2:-10])
