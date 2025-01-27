b12f2c63669a1f9fbbaf74ec52d479dd84d38b2c
san7842 <sandeepjjjjj536@gmail.com>
2025-01-27 23:01:56 +0530

    add

---
+++ typecasting.py
@@ -0,0 +1,31 @@
+# l1=[1,2,3,5,8]
+# l=str(l1)
+# l2=tuple(l)
+# l3=set(l1)
+# l4=dict(l1)
+# print(l)
+# print(type(l))
+# print(l2)
+# print(type(l2))
+# print(l3)
+# print(type(l3))
+# print(l4)
+# print(type(l4))
+# x=input("enter your name")
+# print(x)
+# print(type(x))
+# x= int(input("enter your age"))
+# print(x)
+# print(type(x))# when input string throw error invalid literal for int() with base 10;
+# x=10
+# y=20
+# z=x+y
+# s=x/y
+# t=x//y
+# print(z,type(z))
+# print(s,type(s))
+# print(t,type(t))
+x=5+2j
+y=6+3j
+z=x+y
+print(z,type(z))#(type class <complex)
