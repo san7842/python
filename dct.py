203fcb69ef8298ec721449831a750a9634b0fe19
san7842 <sandeepjjjjj536@gmail.com>
2025-02-03 23:35:38 +0530

    add

--- dct.py
+++ dct.py
@@ -9,7 +9,29 @@
 # print((d))
 # d={'Neeraj':'Neeraj','Tge':37,'Quali':'M.Tech'}
 # print(max(d))
-d={'Neeraj':['Neeraj',20,30],'Tge':37,'Quali':'M.Tech'}
-print(d["Neeraj"][2])
+# d={'Neeraj':['Neeraj',20,30],'Tge':37,'Quali':'M.Tech'}
+# print(d.get('Tge'))
+# print(d.get('Neeraj'))
+# print(d["Neeraj"][2])
+# d['Neeraj']=[10,20]
+# d['name']=[30,40,50]
+# print(d)
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# d.pop('Neeraj')#target any key remove
+# print(d)
+# d.popitem()# remove last items
+# print(d)
+d1=[10,20,30,40,50,60,10,20]
+#d1="sandeep"
+x=dict.fromkeys(d1,12)
+print(x)
+# d.setdefault("Neeraj","Quali")
+# print(d)
+# d.update({"Neeraj":"sandeep","gread":"veer"})
+# print(d)
+
+
 
 
