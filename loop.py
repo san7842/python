1291641d3eb7d32327cd1328a17c94eca083d2cb
san7842 <sandeepjjjjj536@gmail.com>
2025-02-07 21:15:04 +0530

    add

---
+++ loop.py
@@ -0,0 +1,13 @@
+n=int(input("enter any number"))
+c=0
+i=0
+while(i<=n):
+     if(i<n):
+      print(i,end=",")
+     else:
+        print(i)
+     c+=1
+     i+=1
+print(c)
+print("hello",end=",")
+print()
