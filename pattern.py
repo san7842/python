be30c327a57451b3a52801bf4c98cf3ae8a23866
san7842 <sandeepjjjjj536@gmail.com>
2025-02-13 21:37:01 +0530

    add

--- pattern.py
+++ pattern.py
@@ -4,8 +4,103 @@
 #     print("*"*i+' '*(n-i))
 #     i+=1
 # 
+# n=int(input("enter any number"))
+# i=1
+# while(i<=n):
+#     print(' '*(n-i)+" *"*i)
+#     i+=1
+# n=int(input("enter any number"))
+# i=1
+# while(i<=n):
+#     print(' '*(n-i)+'*'*i)
+#     i+=1
+#=================
+# n=int(input("enter any number"))
+# i=0
+# while(i<=n):
+#     print('*'*(n-i)+' '*i)
+#     i+=1
+#=============
+# n=int(input("enter any number"))
+# i=0
+# while(i<=n):
+#     print(' '*(i)+'*'*(n-i))
+#     i+=1
+#========================
+# n=int(input("enter any number"))
+# i=0
+# while(i<=n):
+#     print(' '*i+' * '*(n-i) )
+#     i+=1
+#================== 
+# n=int(input("enter any number"))
+# i=0
+# while(i<n):
+#     print(' * '*(n-i))
+#     i+=1
+# i=2
+# while(i<=n):
+#     print(' * '*i)
+#     i+=1
+
+#=================
+# n=int(input("enter any number"))
+# i=0 
+# while(i<n):
+#     print(' '*i+ '*'*(n-i))
+#     i+=1
+# i=2
+# while(i<=n):
+#     print(' '*(n-i)+'*'*i)
+#     i+=1
+#======================
+# n=int(input("enter any number"))
+# i=0
+# while(i<n):
+#     print(' '*i+ '*'*(n-i))
+#     i+=1
+# i=i-2
+# while(i>=0):
+#     print(' '*i+'*'*(n-i))
+#     i-=1
+#==========================
+
+# n=int(input("enter any number"))
+# i=0
+# while(i<n):
+#     print(' '*i+ ' * '*(n-i))
+#     i+=1
+# i=2
+# while(i<=n):
+#     print(' '*(n-i)+' * '*i)
+#     i+=1
+#==================
 n=int(input("enter any number"))
-i=1
+i=0
 while(i<=n):
-    print(' '*(n-i)+" *"*i)
+    print(' '*(n-i)+'*'*(i))
+    i+=1
+i=1
+while(i<n):
+    print('*'*(n-i)+' '*(i))
+    i+=1
+#========================
+n=int(input("enter any number"))
+i=0
+while(i<=n):
+    print('*'*(i)+' '*(n-i))
+    i+=1
+i=1
+while(i<n):
+    print(' '*(i)+'*'*(n-i))
+    i+=1
+#=====================
+n=int(input("enter any number"))
+i=0
+while(i<=n):
+    print(' '*(n-i) +' * '*(i)+' '*(n-i))
+    i+=1
+i=1
+while(i<n):
+    print( ' '*(i)+' * '*(n-i)+' '*(i))
     i+=1
