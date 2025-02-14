7a1ef15c7d4754f0ea2d103788358bc14f75c755
san7842 <sandeepjjjjj536@gmail.com>
2025-02-14 23:36:08 +0530

    add

---
+++ funct.py
@@ -0,0 +1,18 @@
+# def add(x,y,z):
+#     "for adding of 3 variable"
+#     return x+y+z
+# p=int(input("enter any number"))
+# q=int(input("enter any number"))
+# r=int(input("enter any number"))
+# res=add(p,q,r)
+# print(res)
+# print(add.__doc__)
+def add(x,y,z):
+    "for adding of 3 variable"
+    x+y+z
+p=int(input("enter any number"))
+q=int(input("enter any number"))
+r=int(input("enter any number"))
+res=add(p,q,r)
+print(res)
+print(add.__doc__)
