d8cca84fb30b41ff6373f8110cf8b7de54c971f3
san7842 <sandeepjjjjj536@gmail.com>
2025-02-16 22:28:19 +0530

    add

--- example.py
+++ example.py
@@ -1,3 +1,39 @@
-print("hello"+"world")
-for i in range(1,11):
- print(2*i,end=" ")
+#============example 1===========
+# n=int(input("enter any number"))
+# if(n>0):
+#     print(f'{n} is positive')
+
+#==========example2========
+# n=int(input("enter any number"))
+# if(n>0):
+#     print(f'{n} is positive')
+# else:
+#      print(f'{n} is negative')
+#===========================
+# n=int(input("enter any number"))
+# if(n>0):
+#     print(f'{n} is positive')
+# elif(n<0):
+#     print(f'{n} is negative')
+# else:
+#     print(f'{n} is zero')
+#====================
+#n=eval(input("enter any number"))
+#=========================
+# n=int(input("enter any number"))
+# squre_root(n)
+#======================
+# x=int(input("enter any number"))
+# y=int(input("enter any number"))
+# z=int(input("enter any number"))
+# if(x>y and x>z):
+#     print(f'{x} is greater')
+# elif(y>x and y>z):
+#     print(f'{y} is greater')
+# else:
+#     print(f'{z} is greater')
+#==========================
+hieght=int(input("enter hieght"))
+base=int(input("enter base"))
+area=(hieght*base)/2
+print(area)
--- funct.py
+++ funct.py
@@ -7,12 +7,104 @@
 # res=add(p,q,r)
 # print(res)
 # print(add.__doc__)
-def add(x,y,z):
-    "for adding of 3 variable"
-    x+y+z
-p=int(input("enter any number"))
-q=int(input("enter any number"))
-r=int(input("enter any number"))
-res=add(p,q,r)
+# def add(x,y,z):
+#     "for adding of 3 variable"
+#     x+y+z
+# p=int(input("enter any number"))
+# q=int(input("enter any number"))
+# r=int(input("enter any number"))
+# res=add(p,q,r)
+# print(res)
+# print(add.__doc__)
+# def mult(x,y,z):
+#     "multiplication of two number"
+#     return x*y*z
+# p=int(input("enter any number"))
+# q=int(input("enter any number"))
+# r=int(input("enter any number"))
+# res=mult(p,q,r)
+# print(res)
+#=====position argument=======
+# def add(x,y,z):
+#     print("x:",x)
+#     print("y:",y)
+#     print("z:",z)
+#     return x+y+z
+# p=int(input("enter any number"))
+# q=int(input("enter any number"))
+# r=int(input("enter any number"))
+# res=add(p,q,r)
+# print(res)
+#==============key value argument====
+# def add(x,y,z):
+#     print("x:",x)
+#     print("y:",y)
+#     print("z:",z)
+#     return x+y+z
+# p=int(input("enter any number"))
+# q=int(input("enter any number"))
+# r=int(input("enter any number"))
+# res=add(z=p,y=q,x=r)
+# print(res)
+#===============default  argument===
+# def add(x=0,y=0,z=0):
+#     print("x:",x)
+#     print("y:",y)
+#     print("z:",z)
+#     return x+y+z
+# p=int(input("enter any number"))
+# q=int(input("enter any number"))
+# r=int(input("enter any number"))
+# res=add()
+# print(res)
+#variable-length position argument
+# def add(*n):#(single * args) args as a tuple value hold
+#     print(n)
+#     print(type(n))
+   
+    
+
+# res=add(2,4,8,67)
+# print(res)
+# def add(*args):
+#    sum=0
+#    for i in args:
+#     sum+=i
+#    return sum
+
+# res=add(2,4,6,8,9,10)
+# print(res)
+# n=eval(input("enter any number"))
+# print(n)
+# print(type(n))
+# def add(*args):
+#     sum=0
+#     for i in args:
+#         for j in i:
+#             sum+=j
+#     return sum
+# n=eval(input("enter any number"))
+# res=add(n)
+# print(res)
+
+#======================
+# def add(*args):
+#     sum=0
+#     for i in args:
+#         for j in i:
+#             sum+=j
+#     return sum
+# n=eval(input("enter any number"))
+# res=add(*n)
+# print(res)
+
+#==================
+def add(*args):
+    "add multiple element"
+    sum=0
+    for i in args:
+        sum+=i
+    return sum
+x=eval(input("enter two or more element"))
+res=add(*x)
 print(res)
-print(add.__doc__)
