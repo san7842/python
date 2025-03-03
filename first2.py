28a5e3bdf7b1049fb988ec8ac079e313cb117ef2
san7842 <sandeepjjjjj536@gmail.com>
2025-03-03 23:19:11 +0530

    add

---
+++ first2.py
@@ -0,0 +1,13 @@
+# from  first1 import add
+# x=int(input("enter any"))
+# y=int(input("enter any"))
+# res=add(x,y)
+# print(res)
+from  first1 import add as a #as is keyword by aliashing create sort name of object
+x=int(input("enter any"))
+y=int(input("enter any"))
+res=a(x,y)
+print(res)
+#collection of multiple or singlec file is called module
+#collection of module with init .py is called  package
+#collection of folder with 
